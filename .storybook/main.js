07d4ff17865d59a794792e037d8504ba76130ed2
andrebnassis <andrebnassis@gmail.com>
2021-12-02 17:40:56 -0300

    setup create-react-app with ts, storybook and mui5 with minimal story

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  features: {
+    //Issue related: https://github.com/storybookjs/storybook/issues/16099#issuecomment-964445878
+    //Released solution: https://github.com/storybookjs/storybook/releases/tag/v6.4.0-beta.31
+    emotionAlias: false,
+  },
+}
